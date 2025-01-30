a15a46e74fa36ab91604656554e18d6854765706
Jesus Zavala <betorolitos37@gmail.com>
2025-01-29 21:39:46 -0700

    followMouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const FollowMouse = () => {
+
+   //uso del useState
+   const [enabled, setEnabled] = useState(false)
+   const [position, setPosition] = useState({ x: 0, y: 0 })
+ 
+   //uso del useEffect
+   useEffect(() => {
+     console.log('efecto.....', { enabled })
+ 
+     const handleMove = (event) => {
+       //obtener las posiciones del cursor
+       const { clientX, clientY } = event
+       
+       //setear las posiciones X y Y 
+       setPosition({x: clientX, y: clientY})
+     }
+ 
+     //suscripcion al evento de seguir cursor
+     if (enabled) {
+       window.addEventListener('pointermove', handleMove)
+     }
+ 
+     //cuando el componente se desmonta o cambia las dependencias hace un cleanup
+     return () => {
+       window.removeEventListener('pointermove', handleMove)
+     }
+ 
+   }, [enabled])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={
+      {
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: '0.8',
+        pointerEvents: 'none',
+        left: '-20px',
+        top: '-20px',
+        width: '40px',
+        height: '40px',
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }
+    } />
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} Seguir el Puntero
+    </button>
     </>
   )
+}
+
+function App() {
+  
+  return (
+    <main>
+      <FollowMouse></FollowMouse>
+    </main>
+  )
+
 }
 
 export default App
